de1db54dd85b7df0129ac49bec63939e24ac95bd
Sasha Daszko <sasha.daszko@gmail.com>
2021-12-02 22:18:52 -0300

    Ya tiene boostrap, header y footer

---
+++ ProyectoCoder/ProyectoCoder/AppCoder/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+
+class Jugador(models.Model):
+
+    apellido = models.CharField(max_length=40)
+    numero = models.IntegerField()
+    esBueno = models.BooleanField()
